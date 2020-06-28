c4e08d5108ca35d79477447524bd62129622427e
martin-wachira <martinnwachira@gmail.com>
2020-06-28 17:14:46 +0300

    edit page

--- index.php
+++ index.php
@@ -12,19 +12,27 @@
 <body>
 <nav class="navbar navbar-light colorH">
 <a class="navbar-brand" href="#">
-    <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
-    Bootstrap
+    <img src="https://i.dlpng.com/static/png/6458459_preview.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    <!-- nanovenue -->
   </a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+  <ul class="nav justify-content-end titl">
+  <li class="nav-item">
+    <h4 class="nav-link active" >Home</h4>
+  </li>
+  <li class="nav-item">
+    <h4 class="nav-link active" >Main Stage</h4>  
+</li> 
+</ul>
+  <div class="form-inline">
+    <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+    <span class="btn btn-outline-success my-2 my-sm-0" >Profile</span>
+</div>
 </nav>
 <div class="card-header titl">
     <h1><b> EXHIBITION HALL</b></h1>
 </div>
 <div class="iconC">
-<div class="container">
+    <div class="container">
 <div class="row firstRow">
   <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
   <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>  
@@ -48,8 +56,16 @@
   </div>
   </div>
 <br>
-<div class="footer">
-    Copyright Nanovenue
+<div class="footer">   
+<div class="row">
+    <div class="col-md-9">
+
+        <strong>Copyright Nanovenue &copy;2020</strong>
+    </div>
+<div lass="col-md-3">
+    <p class="pull-right">Social Media</p>
+</div>
+</div>
 </div>
 <script src="assets/bootstrap/jquery.min.js"></script>
 <script src="assets/bootstrap/js/bootstrap.min.js"></script>
