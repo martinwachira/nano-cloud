3ce842c7656cc8ebe7613568111e8adca29bfa48
martin-wachira <martinnwachira@gmail.com>
2020-06-28 16:18:20 +0300

    set boiler plate

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nanovenue</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/custom.css">
+</head>
+<body>
+<nav class="navbar navbar-light colorH">
+<a class="navbar-brand" href="#">
+    <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    Bootstrap
+  </a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</nav>
+<div class="card-header titl">
+    <h1><b> EXHIBITION HALL</b></h1>
+</div>
+<div class="iconC">
+<div class="container">
+<div class="row firstRow">
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>  
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  </div>
+  </div>
+<br>
+  <div class="row secondRow">
+  <div class="col-md-3"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  <div class="col-md-3"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>  
+  <div class="col-md-3"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  <div class="col-md-3"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  </div>
+<br>
+<div class="container">
+<div class="row firstRow">
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>  
+  <div class="col-md-4"><img src="https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png" alt=""> </div>
+  </div>
+  </div>
+  </div>
+<br>
+<div class="footer">
+    Copyright Nanovenue
+</div>
+<script src="assets/bootstrap/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://unpkg.com/ionicons@5.0.0/dist/ionicons.js"></script>
+</body>
+</html>
