fdfaa239b981ee97c87254c896a76799d3921b21
martin-wachira <martinnwachira@gmail.com>
2020-07-23 22:57:59 +0300

    made changes

---
+++ index1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nanovenue</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/custom.css">
+</head>
+
+
+<body>
+    
+<nav class="navbar navbar-light colorH">
+<a class="navbar-brand" href="#">
+    <img src="https://i.dlpng.com/static/png/6458459_preview.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    <!-- nanovenue -->
+  </a>
+  <ul class="nav justify-content-end titl">
+  <li class="nav-item">
+    <h4 class="nav-link active" >Home</h4>
+  </li>
+  <li class="nav-item">
+    <h4 class="nav-link active" >Main Stage</h4>  
+</li> 
+</ul>
+  <div class="form-inline">
+    <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+    <span class="btn btn-outline-success my-2 my-sm-0" >Profile</span>
+</div>
+</nav>
+<div class="card-header titl">
+    <h1><b> EXHIBITION HALL</b></h1>
+</div>
+    <div class = "stand">
+        <div class="front"><img src=https://cdn2.iconfinder.com/data/icons/traditional-marketing-volume-1/500/Exhibition-512.png></div>
+        <div class="back">
+            <div class="details">
+                <h2>Company <br> Name </h2>
+                <div><button> Go to...</button></div>
+            </div>
+        </div>
+        
+    </div>
+
+    
+
+</body>
+
+</html>
---
+++ index2.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+		<title>Nanovenue</title>
+		<meta name="description" content="A shading effect for 3D transformed elements" />
+		<meta name="keywords" content="css3, transforms, shadow, shading, 3d, box shadow" />
+		<meta name="author" content="Codrops" />
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link rel="stylesheet" type="text/css" href="assets/css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="assets/css/demo.css" />
+		<link rel="stylesheet" href="assets/css/newComponent.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/newCustom.css">
+		<script src="assets/js/modernizr.custom.js"></script>
+        
+	</head>
+	<body>
+ <div class="container1">
+  <nav class="navbar navbar-light colorH">
+  <a class="navbar-brand" href="#">
+    <img src="https://i.dlpng.com/static/png/6458459_preview.png" width="30" height="30" class="d-inline-block align-top" alt="" loading="lazy">
+    <!-- nanovenue -->
+  </a>
+  <ul class="nav justify-content-end titl">
+  <li class="nav-item">
+    <h4 class="nav-link active" >Home</h4>
+  </li>
+  <li class="nav-item">
+    <h4 class="nav-link active" >Main Stage</h4>  
+  </li> 
+  </ul>
+  <div class="form-inline">
+    <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> -->
+    <span class="btn btn-outline-info my-2 my-sm-0" style="background-color:grey; color:whitesmoke">Profile</span>
+  </div>
+  </nav>
+    
+		<div class="container">
+			
+			<header>
+            <div class="card-header titl">
+                <h1><b> EXHIBITION HALL</b></h1>
+            </div>  
+			</header>
+			<div class="wrapper">
+				<ul class="stage clearfix">
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+									This is the first company enrolled in our application.
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"> </span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                  
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"> </span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"> </span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+                    <li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"> </span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+                    <li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"></span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+                    <li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"> </span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"></span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                               
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"></span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                                
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"></span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+					<li class="scene">
+						<div class="movie" onclick="return true">
+							<div class="poster"></div>
+							<div class="info">
+								<header>
+									<h1>Company Name</h1>
+									<p>
+                               
+								</p>
+									<span class="year"></span>
+									<span class="rating"></span>
+									<span class="duration"></span>
+								</header>
+							</div>
+						</div>
+					</li>
+
+				</ul>
+			</div><!-- /wrapper -->
+		</div><!-- /container -->
+     </div>
+    </body>
+    
+    <footer>
+    <div class="row">
+    <div class="col-md-9">
+
+        <strong>Copyright Nanovenue &copy;2020</strong>
+    </div>
+<div class="col-md-3">
+	<img src="assets/images/fb.png" width="50px" height="50px">
+	<img src="assets/images/ig.png" width="50px" height="50px">
+	<img src="assets/images/twitter.png" width="50px" height="50px">
+</div>
+</div>
+    </footer>
+</html>
